2282ceda625d8109e01b5051ae91028630200d21
kuky <kukyxingshi@163.com>
2018-02-25 16:03:27 +0800

    解决 Android 传字符串给 JS 时调用 JS 方法失效问题

--- app/src/main/java/com/kuky/base/view/activity/JsContractActivity.kt
+++ app/src/main/java/com/kuky/base/view/activity/JsContractActivity.kt
@@ -3,13 +3,17 @@ package com.kuky.base.view.activity
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.text.InputFilter
+import android.text.TextUtils
 import android.view.View
 import android.webkit.*
 import com.kuky.base.R
 import com.kuky.base.databinding.ActivityJsContractBinding
 import com.kuky.baselib.baseClass.BaseActivity
+import com.kuky.baselib.baseUtils.LogUtils
 import com.kuky.baselib.baseUtils.ToastUtils
 
 class JsContractActivity : BaseActivity<ActivityJsContractBinding>() {
@@ -23,11 +27,13 @@ class JsContractActivity : BaseActivity<ActivityJsContractBinding>() {
         return R.layout.activity_js_contract
     }
 
-    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface", "AddJavascriptInterface")
+    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
     override fun initActivity(savedInstanceState: Bundle?) {
         mViewBinding.js = this@JsContractActivity
+        mViewBinding.topEt.filters = arrayOf(InputFilter.LengthFilter(20))
         mViewBinding.contentWeb.settings.javaScriptEnabled = true
         mViewBinding.contentWeb.settings.domStorageEnabled = true
+        mViewBinding.contentWeb.settings.setSupportZoom(false)
         mViewBinding.contentWeb.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view!!.loadUrl(url!!)
@@ -47,6 +53,19 @@ class JsContractActivity : BaseActivity<ActivityJsContractBinding>() {
                 mViewBinding.pro.visibility = if (newProgress == 0 || newProgress == 100) View.GONE else View.VISIBLE
                 mViewBinding.pro.progress = newProgress
             }
+
+            /**
+             * 修改 JS alert 对话框样式为 Android 原生样式
+             */
+            override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+                val builder = AlertDialog.Builder(this@JsContractActivity)
+                        .setTitle("Alert")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok) { _, _ -> result!!.confirm() }
+                        .setCancelable(false)
+                builder.create().show()
+                return true
+            }
         }
         mViewBinding.contentWeb.loadUrl("file:///android_asset/web.html")
         /**
@@ -61,12 +80,24 @@ class JsContractActivity : BaseActivity<ActivityJsContractBinding>() {
     }
 
     fun ktCallJs(view: View) {
-        mViewBinding.contentWeb.loadUrl("javascript:ktcalljs()")
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT)
+            mViewBinding.contentWeb.loadUrl("javascript:ktcalljs()")
+        else
+            mViewBinding.contentWeb.evaluateJavascript("javascript:ktcalljs()") { result -> LogUtils.e(result ?: "null") }
     }
 
     fun ktCallJsWithParam(view: View) {
-        val arg = "https://github.com/kukyxs"
-        mViewBinding.contentWeb.loadUrl("javascript:ktcalljswith($arg)")
+        if (!TextUtils.isEmpty(mViewBinding.topEt.text.toString())) {
+            val arg = "\'${mViewBinding.topEt.text}\'"
+            mViewBinding.topEt.setText("")
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+                mViewBinding.contentWeb.loadUrl("javascript:ktcalljswith($arg)")
+            } else {
+                mViewBinding.contentWeb.evaluateJavascript("javascript:ktcalljswith($arg)") { result -> LogUtils.e(result ?: "null") }
+            }
+        } else {
+            ToastUtils.showToast(this@JsContractActivity, resources.getString(R.string.param_hint))
+        }
     }
 
     override fun onBackPressed() {
